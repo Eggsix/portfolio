00f66266255bfd47d927ae78e18087de80d1ef8c
Preston <plphan206@yahoo.com>
2015-11-15 23:54:37 -0800

    heroku mongo test

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
-
-mongoose.connect('mongodb://localhost/portfolio');
+// mongoose.connect('mongodb://localhost/portfolio');
+mongoose.connect('mongodb://Eggsix:Eggs6767@ds053954.mongolab.com:53954/eggsix');
 
 var models_path = __dirname + '/../models';
 
