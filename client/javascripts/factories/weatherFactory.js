c4d9e6f93526c7a2c190661c03df4275cd847a02
Preston <plphan206@yahoo.com>
2016-01-19 20:51:32 -0800

    new weather api key

--- client/javascripts/factories/weatherFactory.js
+++ client/javascripts/factories/weatherFactory.js
@@ -1,7 +1,7 @@
 myApp.factory('weatherFactory', function ($http) {
 	return {
 		getWeather: function (callback) {
-			$http.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle,us&appid=bea022332329a049f865b6522d01b133").then(function(data) {
+			$http.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle,us&appid=717fbf698bb2dd90997c882680d12571").then(function(data) {
 				callback(data);
 			})
 		}
