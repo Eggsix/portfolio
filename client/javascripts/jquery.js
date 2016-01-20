ac0ab1910914d2a0add7abf0c4893b15a7b18bb0
Preston <plphan206@yahoo.com>
2016-01-19 22:31:07 -0800

    fixed weather api

--- client/javascripts/jquery.js
+++ client/javascripts/jquery.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
+	$.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle,us&appid=717fbf698bb2dd90997c882680d12571", function(res) {
+		var temp = res.main.temp,
+			weather_city = res.name,
+			weather_condition = res.weather[0].description,
+			ferhenheit = Math.floor(temp * 9/5 - 459.67);
+			document.getElementById("weather_city").innerHTML = weather_city;
+			document.getElementById("weather_condition").innerHTML = weather_condition;
+			document.getElementById("ferhenheit").innerHTML = ferhenheit;
+	});
+	//slider
 	var width = 600;
 	var animationSpeed = 1000;
 	var pause = 5000;
 	var currentSlide = 1;
-	
 	var interval;
-	//set interval
-
 	function startSlider(){
 		interval = setInterval(function () {
 			var slider = $('#slider');
@@ -29,15 +36,6 @@ $(document).ready(function() {
 
 	startSlider();
 
-	$('#slider').on('mouseenter', pauseSlider).on('mouseleave', startSlider);
-		//animate margin-left
-		//if it's last slide, go to position 1 (0px);
-
-
-		//listen for mouseenter and pause
-		
-		//resume on mouseleave
-
-	//materialize nave bar collapse
+	//materialize side nav
 	$(".button-collapse").sideNav();
 });
